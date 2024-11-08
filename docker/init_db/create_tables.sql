8dd8ab9d08933d19d3ffd803b4d522671663452f
omnomdom <v.rabetskii@gmail.com>
2024-11-09 02:25:56 +0700

    add: добавил атрибут типа события

--- docker/init_db/create_tables.sql
+++ docker/init_db/create_tables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE chat_messages
 (
     ID       SERIAL PRIMARY KEY,
+    event    VARCHAR(30) NOT NULL,
     username VARCHAR(50) NOT NULL,
     message  TEXT                ,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
